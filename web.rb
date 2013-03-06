c62056919412e857a6c6d8bf11b69bcbe7eaacee
Brad Humphrey <brad@instructure.com>
2013-03-06 13:35:25 -0700

    more debugging code

--- web.rb
+++ web.rb
@@ -7,24 +7,34 @@ get '/' do
 end
 
 get '/return_content' do
-  return session.to_yaml unless session.key? :launch_presentation_return_url
+  return 'Invalid return URL' unless session.key? :launch_presentation_return_url
   return 'Expected box view URL' unless params.key? 'view_url'
 
   require 'URI'
   require 'open-uri'
 
+  return params['test1'] if params.key? 'test1'
+
   redirect_url = URI.parse session[:launch_presentation_return_url]
+
+  return params['test2'] if params.key? 'test2'
 
   url_scheme = request.ssl? ? "https" : "http"
   domain = request.env['SERVER_NAME']
   tool_url = "#{url_scheme}://#{request.env['HTTP_HOST']}/?box_file_key=#{params['view_url'][/\/s\/(.*$)/,1]}"
 
+  return params['test3'] if params.key? 'test3'
+
   text_param = (params.key?('file_name') ? "&text=#{params['file_name']}" : "")
+
+  return params['test4'] if params.key? 'test4'
 
   redirect_url.query = "embed_type=basic_lti&url=#{CGI::escape(tool_url)}#{text_param}"
 
   session.clear
   session[:canvas_redirect] = redirect_url.to_s
+
+  return params['test5'] if params.key? 'test5'
 
   redirect "#{params['redirect_to_box_url']}&status=success&message=Almost%20there!%20Your%20file%20will%20be%20in%20canvas%20shortly."
 end
