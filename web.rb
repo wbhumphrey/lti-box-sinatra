4007a163ebf56eba723c5b26e8e8d233f9f23cc8
Brad Humphrey <brad@instructure.com>
2013-03-06 12:54:42 -0700

    bug fixes

--- web.rb
+++ web.rb
@@ -7,32 +7,37 @@ end
 
 get '/return_content' do
   return 'Invalid return URL' unless session.key? :launch_presentation_return_url
-  return 'Expected box view URL' unless params.key? :view_url
+  return 'Expected box view URL' unless params.key? 'view_url'
 
   require 'URI'
-  require 'open_uri'
+  require 'open-uri'
 
   redirect_url = URI.parse session[:launch_presentation_return_url]
 
   url_scheme = request.ssl? ? "https" : "http"
   domain = request.env['SERVER_NAME']
-  tool_url = "#{url_scheme}://#{request.env['HTTP_HOST']}/?box_file_key=#{box_params[:view_url][/\/s\/.*$/,1]}"
+  tool_url = "#{url_scheme}://#{request.env['HTTP_HOST']}/?box_file_key=#{params['view_url'][/\/s\/(.*$)/,1]}"
 
-  title_param = (params.key?(:file_name) ? "&title=#{params[:file_name]}" : "" )
+  text_param = (params.key?('file_name') ? "&text=#{params['file_name']}" : "")
 
-  redirect_url.query = "url=#{URI::encode(tool_url)}#{title_param}"
+  redirect_url.query = "embed_type=basic_lti&url=#{CGI::escape(tool_url)}#{text_param}"
 
   session.clear
   session[:canvas_redirect] = redirect_url.to_s
+
+  redirect "#{params['redirect_to_box_url']}&status=success&message=Almost%20there!%20Your%20file%20will%20be%20in%20canvas%20shortly."
 end
 
 post '/' do
-  session[:launch_presentation_return_url] = params[:launch_resentation_return_url]
+  session.clear
+  session[:launch_presentation_return_url] = params[:launch_presentation_return_url]
 
   if params[:box_file_key]
-    iframe_src = "https://www.box.com/embed_widget/000415338141/s/#{params[:box_file_key]}?promoted_app_ids=2288"
-  else
+    iframe_src = "https://www.box.com/embed_widget/000415338140/s/#{params[:box_file_key]}"
+  elsif
     iframe_src =  "https://www.box.com/embed_widget/000415338141/files/0/f/0?promoted_app_ids=2288"
+  else
+    iframe_src =  "https://www.box.com/embed_widget/000415338140/files/0/f/0"
   end
 
   erb :index, :locals => { :iframe_src => iframe_src }
@@ -43,6 +48,6 @@ get '/canvas_redirect' do
 
   content_type :json
   ret_value = (session.key?(:canvas_redirect) ? { :canvas_redirect => session[:canvas_redirect] } :  {})
-  session.clear
+  session.clear unless ret_value.empty?
   ret_value.to_json
 end
